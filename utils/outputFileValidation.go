197506103d6e2eb7458b1497e1ea1dbf5ecbb1df
onyango-granton <grantononyango@gmail.com>
2024-08-12 09:52:32 +0300

    del: remove unneccesary file

--- utils/outputFileValidation.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-/*IsValid functions checks if the output file name is same as the banner files */
-func IsValid(s string) bool {
-	if s == "standard.txt" || s == "shadow.txt" || s == "thinkertoy.txt" {
-		return true
-	}
-	return false
-}
